7d4c83b7bb5f7d2054421090a63d2831d77ceaf3
Vikas Sutar <vikassutarfsd1020@gmail.com>
2025-07-10 23:36:39 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+a="vikas"
+print(a)
+
+#multiple string
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
